24f31978f8971f560fc17c23cea25d2c017ac775
Ravi Chandak <ravi.chandak901@gmail.com>
2015-12-06 17:19:50 +0530

    links corrected

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
- 
+  $scope.toSection1 = function(id){
+  	$mdSidenav(1).toggle();
+  };
 }]);
